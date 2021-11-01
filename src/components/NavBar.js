d5c7cf84a35ce4cb7c7fcb3115210a1bae20a66d
avi-11 <avi1109cool@gmail.com>
2021-11-01 23:46:46 +0530

    --Navbar Dropdown--

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,56 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="p-3 w-full h-15 shadow-md flex justify-between navbar">
-      <div className="bg-blue-500">LOGO</div>
-      <div className="pl-96">
-        <NavLink exact to="/" className="text-2xl px-7">
-          Home
-        </NavLink>
+    <div>
+      <nav className="p-3 w-full h-15 shadow-md flex justify-between navbar">
+        <div className="bg-blue-500">LOGO</div>
+        <div className="pl-96">
+          <NavLink exact to="/" className="text-2xl px-7">
+            Home
+          </NavLink>
 
-        <NavLink exact to="/courses" className="text-2xl px-7">
-          Courses
-        </NavLink>
+          <NavLink exact to="/courses" className="text-2xl px-7">
+            Courses
+          </NavLink>
 
-        <NavLink exact to="/progress" className="text-2xl px-7">
-          Progress
-        </NavLink>
+          <NavLink exact to="/progress" className="text-2xl px-7">
+            Progress
+          </NavLink>
 
-        <NavLink exact to="/Qna" className="text-2xl px-7">
-          Qna
-        </NavLink>
+          <NavLink exact to="/Qna" className="text-2xl px-7">
+            Qna
+          </NavLink>
+        </div>
+        <div className=" profile-dropdown flex">
+          <div className="circle"></div>
+          <div className="text-m profile">profile</div>
+          <i class="fas fa-sort-down fa-lg icon"></i>
+        </div>
+      </nav>
+      <div className="bg-blue-500 inline-block">
+        <div>
+          <i class="fas fa-sort-down fa-lg icon"></i>
+          <Link>Profile</Link>
+        </div>
+        <div>
+          <i class="fas fa-sort-down fa-lg icon"></i>
+          <Link>Profile</Link>
+        </div>
+        <div>
+          <i class="fas fa-sort-down fa-lg icon"></i>
+          <Link>Profile</Link>
+        </div>
+        <div>
+          <Link>
+            <button className="bg-white-500">SignUp</button>
+          </Link>
+        </div>
       </div>
-      <div className=" profile-dropdown flex">
-        <div className="circle"></div>
-        <div className="text-m profile">profile</div>
-        <i class="fas fa-sort-down fa-lg icon"></i>
-      </div>
-    </nav>
+    </div>
   );
 }
 
