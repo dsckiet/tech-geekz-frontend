0ad46fcf59cd0fa727e12ee5e5630b57cb96a00c
avi-11 <avi1109cool@gmail.com>
2021-11-02 11:24:51 +0530

    --Navbar Dropdown Updated--

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function NavBar() {
+  const [dropdown, setDropdown] = useState(false);
+
   return (
     <div>
       <nav className="p-3 w-full h-15 shadow-md flex justify-between navbar">
@@ -25,28 +28,37 @@ function NavBar() {
             Qna
           </NavLink>
         </div>
-        <div className=" profile-dropdown flex">
+        <div
+          className="profile-dropdown flex"
+          onClick={() => setDropdown(!dropdown)}
+        >
           <div className="circle"></div>
-          <div className="text-m profile">profile</div>
-          <i class="fas fa-sort-down fa-lg icon"></i>
+          <div className="text-m profile px-1">profile</div>
+          <i
+            class={
+              dropdown
+                ? "fas fa-sort-down fa-lg icon rotate-180"
+                : "fas fa-sort-down fa-lg icon"
+            }
+          ></i>
         </div>
       </nav>
-      <div className="bg-blue-500 inline-block">
-        <div>
-          <i class="fas fa-sort-down fa-lg icon"></i>
-          <Link>Profile</Link>
+      <div className={dropdown ? "inline-block dropdown" : "hide-dropdown"}>
+        <div className="px-1 py-1.5">
+          <i class="far fa-user-circle fa-md px-1"></i>
+          <Link className="tracking-wide px-1">Profile</Link>
         </div>
-        <div>
-          <i class="fas fa-sort-down fa-lg icon"></i>
-          <Link>Profile</Link>
+        <div className="px-1 py-1.5">
+          <i class="fas fa-cog fa-md px-1"></i>
+          <Link className="tracking-wide px-1">Setting</Link>
         </div>
-        <div>
-          <i class="fas fa-sort-down fa-lg icon"></i>
-          <Link>Profile</Link>
+        <div className="px-1 py-1.5">
+          <i class="far fa-question-circle fa-md px-1"></i>
+          <Link className="tracking-wide px-1">Help</Link>
         </div>
-        <div>
+        <div className="p-1">
           <Link>
-            <button className="bg-white-500">SignUp</button>
+            <button className="bg-white-500 login">SignUp</button>
           </Link>
         </div>
       </div>
